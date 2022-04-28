dd44f8d234a1ff91076d48b817c67a439d23b717
do-not-do-that <rl7968@naver.com>
2022-04-28 11:36:49 +0900

    feat : products platform, client_platform, ads 모델 생성
    products app 내 platform, client_platform, ads 모델 생성 후 migration

--- products/models.py
+++ products/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+from advertisers.models import Client
+
+class Platform(models.Model):
+    class Meta:
+        db_table = 'platform'
+    
+    name = models.CharField(max_length=100)
+
+class ClientPlatform(models.Model):
+    class Meta:
+        db_table = 'client_platform'
+    
+    client_id = models.ForeignKey(Client, on_delete=models.CASCADE)
+    platform_id = models.ForeignKey(Platform, on_delete=models.CASCADE)
+
+class Ads(models.Model):
+    class Meta:
+        db_table = 'ads'
+
+    client_platform_id = models.ForeignKey(ClientPlatform, on_delete=models.CASCADE)
+    uid = models.CharField(max_length=200)
+    date = models.DateField()
+    cost = models.IntegerField()
+    imporession = models.IntegerField()
+    click = models.IntegerField()
+    conversion = models.IntegerField()
+    cv = models.IntegerField()
+    ctr = models.FloatField()
+    roas = models.FloatField()
+    cpc = models.FloatField()
+    cvr = models.FloatField()
+    cpa = models.FloatField()
