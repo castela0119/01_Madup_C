ee6918a7cc94eabce4858d31e7e1d0bec68efb6f
do-not-do-that <rl7968@naver.com>
2022-04-28 21:28:57 +0900

    feat : advertisers client 모델 수정
    advertisers app 내 client 모델 수정 후 migration

--- advertisers/models.py
+++ advertisers/models.py
@@ -8,4 +8,7 @@ class Client(models.Model):
     class Meta:
         db_table = 'client'
 
-    name = models.CharField(max_length=100)
+    client_number = models.PositiveIntegerField(default=0)
+    name = models.CharField(max_length=32, default=None)
+    contact_number = models.CharField(max_length=20, default=None)
+    email = models.EmailField(max_length=128, default=None)
