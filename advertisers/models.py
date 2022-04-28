f899957675d92bcf975acb34a9a4b9b82724ca14
do-not-do-that <rl7968@naver.com>
2022-04-28 11:34:04 +0900

    feat : advertisers client 모델 생성
    advertisers app 내 client 모델 생성 후 migration

--- advertisers/models.py
+++ advertisers/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Client(models.Model):
+    class Meta:
+        db_table = 'client'
+
+    name = models.CharField(max_length=100)
