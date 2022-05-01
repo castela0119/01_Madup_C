d51390c6495d224927e5ab68c902e486c73c6713
AshbeeKim <ksb.forest@gmail.com>
2022-05-01 23:48:40 +0900

    feat : BasePermission을 가진 UserTest 생성
    이번 과제에선 제대로 된 TDD를 구축하지 못했으나, 다음에 도입할 수 있도록 UserTest 생성

--- products/tests/test_views.py
+++ products/tests/test_views.py
@@ -1,10 +1,30 @@
-import json
 from datetime import date
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+from rest_framework.permissions import BasePermission
 
 from products.views import AdsAPIView
+
+
+# user test를 위한 클래스
+class UserTest(BasePermission):
+    """
+    Assignee : 김수빈
+    Reviewer : -
+    """
+    def get_permission(self, request, view):
+        return request.user and request.user.is_superuser
+
+
+    def get_object_permission(self, request, view, object):
+        if request.user:
+            if request.user.is_superuser:
+                return True
+            else:
+                return object == request.user
+        else:
+            return False
 
 
 class AdsAPIViewTest(APITestCase):
